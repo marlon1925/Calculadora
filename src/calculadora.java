13a20db3281aa34631470152eeab7021990b7b72
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-01-22 18:00:38 -0500

    Acción de los Botones

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,8 +1,8 @@
-import javax.jws.Oneway;
+import javax.lang.model.element.NestingKind;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class calculadora {
+public class calculadora extends javax.swing.JFrame {
     private JPanel panel1;
     private JButton botonCero;
     private JButton botonUno;
@@ -29,126 +29,181 @@ public class calculadora {
 
     public calculadora() {
 
-
         botonCero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"0");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonUno.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"1");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText() + digito);
             }
         });
+
 
         botonDos.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"2");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonTres.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"3");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
+
 
         botonCuatro.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"4");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonCinco.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"5");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonSeis.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"6");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonSiete.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"7");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonOcho.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(txtOperaciones.getText()+"8");
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
-
+        botonNueve.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // obtener el valor del boton
+                String digito = e.getActionCommand();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
+            }
+        });
 
         botonSuma.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText("+");
+                // obtener el valor del boton
+                String digito = botonSuma.getText();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText() + digito);
             }
         });
 
         botonResta.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText("-");
+                //obtener el valor del boton
+                String digito = botonResta.getText();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonMulti.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText("*");
+                //obtener el valor del boton
+                String digito = botonMulti.getText();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonDiv.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText("/");
-            }
-        });
-
-        botonPunto.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                txtOperaciones.setText(".");
+                //obtener el valor del boton
+                String digito = botonDiv.getText();
+                // agregar el valor al JLabel
+                txtOperaciones.setText(txtOperaciones.getText()+digito);
             }
         });
 
         botonBorrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // limpiar el valor del JLabel txtOperaciones
                 txtOperaciones.setText("");
+                // limpiar el valor del JLabel txtResultado
+                txtResultado.setText("0");
             }
         });
 
+        botonPunto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!txtOperaciones.getText().contains(".")) {
+                    txtOperaciones.setText(txtOperaciones.getText() + ".");
+                }
+            }
+        });
 
     }
 
-
-
     public static void main (String[]args){
-        JFrame frame = new JFrame("v1");
+        JFrame frame = new JFrame("Calculadora");
         frame.setContentPane(new calculadora().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-
     }
+
 }
+
+
+
+
