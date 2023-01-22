4242d6f1342117f78d51d397fe9244d4a05b3743
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-01-22 13:35:41 -0500

    Primera parte diseño

--- src/calculadora.java
+++ src/calculadora.java
@@ -4,30 +4,37 @@ import java.awt.event.ActionListener;
 
 public class calculadora {
     private JPanel panel1;
-    private JButton a0Button;
-    private JButton a1Button;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JButton button13;
+    private JButton botonCero;
+    private JButton botonUno;
+    private JButton botonDos;
+    private JButton botonTres;
+    private JButton botonCuatro;
+    private JButton botonCinco;
+    private JButton botonSeis;
+    private JButton botonIgual;
+    private JButton botonNueve;
+    private JButton botonOcho;
+    private JButton botonSiete;
+    private JButton botonPunto;
     private JLabel mostrar;
+    private JButton botonC;
+    private JButton botonDiv;
+    private JButton botonMulti;
+
+    private JButton botonRes;
+    private JPanel JPEspacioP;
+    private JLabel txtOperaciones;
+    private JLabel txtResultado;
+    private JButton botonSuma;
 
     public calculadora() {
-        a0Button.addActionListener(new ActionListener() {
+        botonCero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mostrar.setText("0");
             }
         });
-        a1Button.addActionListener(new ActionListener() {
+        botonUno.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mostrar.setText("1");
@@ -41,7 +48,12 @@ public class calculadora {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
     }
 
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
