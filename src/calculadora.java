ab789cc4f900f905f261903944a140db14ee9630
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-01-22 16:31:58 -0500

    Accion botones

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,7 +1,7 @@
+import javax.jws.Oneway;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class calculadora {
     private JPanel panel1;
     private JButton botonCero;
@@ -9,7 +9,6 @@ public class calculadora {
     private JLabel mostrar;
 
     private JPanel JPEspacioP;
-    private JLabel txtOperaciones;
     private JLabel txtResultado;
     private JButton botonTres;
     private JButton botonDos;
@@ -23,23 +22,127 @@ public class calculadora {
     private JButton botonSuma;
     private JButton botonPunto;
     private JButton botonIgual;
+    private JLabel txtOperaciones;
+    private JButton botonMulti;
+    private JButton botonDiv;
+    private JButton botonBorrar;
 
     public calculadora() {
+
+
         botonCero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                mostrar.setText("0");
+                txtOperaciones.setText(txtOperaciones.getText()+"0");
             }
         });
+
         botonUno.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                mostrar.setText("1");
+                txtOperaciones.setText(txtOperaciones.getText()+"1");
             }
         });
+
+        botonDos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"2");
+            }
+        });
+
+        botonTres.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"3");
+            }
+        });
+
+        botonCuatro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"4");
+            }
+        });
+
+        botonCinco.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"5");
+            }
+        });
+
+        botonSeis.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"6");
+            }
+        });
+
+        botonSiete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"7");
+            }
+        });
+
+        botonOcho.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(txtOperaciones.getText()+"8");
+            }
+        });
+
+
+
+        botonSuma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText("+");
+            }
+        });
+
+        botonResta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText("-");
+            }
+        });
+
+        botonMulti.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText("*");
+            }
+        });
+
+        botonDiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText("/");
+            }
+        });
+
+        botonPunto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText(".");
+            }
+        });
+
+        botonBorrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtOperaciones.setText("");
+            }
+        });
+
+
     }
 
-    public static void main(String[] args) {
+
+
+    public static void main (String[]args){
         JFrame frame = new JFrame("v1");
         frame.setContentPane(new calculadora().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,6 +151,4 @@ public class calculadora {
 
 
     }
-
-
 }
